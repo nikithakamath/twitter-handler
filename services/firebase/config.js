aae0631055be0087a5fabfd063818ecee8d8ed05
Nikitha Kamath <nikithakamathk@gmail.com>
2019-10-28 10:40:00 +0530

    Version 2.5: minor updates

--- services/firebase/config.js
+++ services/firebase/config.js
@@ -2,19 +2,20 @@
 
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./key.json');
+/*const serviceAccount = require('key.json');
 // Firebase Project configurations
 const firebaseApp = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: process.env.FIREBASE_DB_URL,
-});
+});*/
+const key = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n');
 
-/*const firebaseApp = admin.initializeApp({
+const firebaseApp = admin.initializeApp({
   credential: admin.credential.cert({
   "type": "service_account",
   "project_id": process.env.FIREBASE_PROJECT_ID,
   "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-  "private_key": process.env.FIREBASE_PRIVATE_KEY,
+  "private_key": key,
   "client_email": process.env.FIREBASE_CLIENT_EMAIL,
   "client_id": process.env.FIREBASE_CLIENT_ID,
   "auth_uri": process.env.FIREBASE_AUTH_URI,
@@ -23,6 +24,6 @@ const firebaseApp = admin.initializeApp({
   "client_x509_cert_url": process.env.FIREBASE_CLIENT_CERT
   }),
   databaseURL: process.env.FIREBASE_DB_URL,
-});*/
+});
 
 module.exports.firebaseApp = firebaseApp;
